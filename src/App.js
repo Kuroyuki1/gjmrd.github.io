04963e81876b00a36dad1e4ba8dd7df02ebf0647
magomedovgh@gmail.com <magomedovgh@gmail.com>
2018-09-13 16:41:41 +0300

    now on react

---
+++ src/App.js
@@ -0,0 +1,59 @@
+import React, { Component } from 'react';
+import Card from './components/Card';
+import social from './json/social.json';
+import otherSocial from './json/otherSocial.json';
+import './css/bootstrap.min.css';
+import './App.css';
+
+
+
+class App extends Component {
+  state = {
+    locale: 'ru',
+  };
+  switchLanguage = (locale) => {
+    this.setState({ locale });
+    console.log(locale);
+  };
+  setRu = () => {
+    this.switchLanguage('ru');
+  }
+  setEn = () => {
+    this.switchLanguage('en');
+  }
+  render() {
+    const { locale } = this.state;
+    const title = locale === 'ru' ? 'Социальные сети' : 'For communication';
+    const otherTitle = locale === 'ru' ? 'Другие соц. сети' : 'Other social';
+    return (
+      <div className="content container">
+        <div className="lang col-md-4">
+          <div>
+            <h4>Language</h4>
+          </div>
+          <div className="lang-switch ru" onClick={this.setRu} >Русский</div>
+          <div className="lang-switch en" onClick={this.setEn}>English</div>
+        </div>
+        <div className="social">
+          <h1>{title}</h1>
+          <div className="row">
+            {social.map((card) => {
+              return <Card key={card.name} card={card} />;
+            })}
+          </div>
+        </div>
+        <div className="social other-social">
+          <h1>{otherTitle}</h1>
+          <div className="row">
+            {otherSocial.map((card) => {
+              return <Card key={card.name} card={card} />;
+            })}
+          </div>
+        </div>
+      </div>
+    
+    );
+  }
+}
+
+export default App;
---
+++ src/components/Card.jsx
@@ -0,0 +1,36 @@
+import React, { Component } from 'react';
+
+class Card extends Component {
+  render() {
+
+    const { name, link, label, logo, activity, comment } = this.props.card;
+
+    return (
+      <div className="col-md-4">
+        <div className="card">
+          <div className="card-title">
+            {name}
+          </div>
+          <div className="card-body">
+            <div className="card-logo">
+              <img src={logo} alt="" />
+            </div>
+            <div className="card-content">
+              <div className="card-label">
+                {label}
+              </div>
+              <div className="card-activity">
+                {activity}
+              </div>
+              <div className="card-comment">
+                {comment}
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    );
+  }
+}
+
+export default Card;
