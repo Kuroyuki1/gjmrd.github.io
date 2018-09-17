84b8a182c39b1106cdfbcc2f636ee0bf8bbf5b8b
magomedovgh@gmail.com <magomedovgh@gmail.com>
2018-09-17 07:54:51 +0300

    testing with new method

--- src/App.js
+++ src/App.js
@@ -4,6 +4,17 @@ import Description from './components/Description';
 import SocialBlock from './components/SocialBlock';
 import FooterBlock from './components/FooterBlock';
 
+/*
+import ruLocale from './json/localization/ru-Ru.json';
+import enLocale from './json/localization/en-En.json';
+
+import ruSocial from './json/cards/ru/social.json';
+import ruOtherSocial from './json/cards/ru/otherSocial.json';
+
+import enSocial from './json/cards/en/social.json';
+import enOtherSocial from './json/cards/en/otherSocial.json';
+*/
+
 import axios from 'axios';
 
 import './css/bootstrap.min.css';
@@ -25,14 +36,14 @@ class App extends Component {
   }
 
   componentWillMount() {
-    axios.get('/src/json/cards/ru/social.json').then(response => this.state.ruSocial = response.data);
-    axios.get('/src/json/cards/ru/otherSocial.json').then(response => this.state.ruOtherSocial = response.data);
+    axios.get('/src/json/cards/ru/social.json').then(response => this.state.ruSocial = response.data).catch(error => console.log(error));
+    axios.get('/src/json/cards/ru/otherSocial.json').then(response => this.state.ruOtherSocial = response.data).catch(error => console.log(error));
 
-    axios.get('/src/json/cards/en/social.json').then(response => this.state.enSocial = response.data);
-    axios.get('/src/json/cards/en/otherSocial.json').then(response => this.state.enOtherSocial = response.data);
+    axios.get('/src/json/cards/en/social.json').then(response => this.state.enSocial = response.data).catch(error => console.log(error));
+    axios.get('/src/json/cards/en/otherSocial.json').then(response => this.state.enOtherSocial = response.data).catch(error => console.log(error));
 
-    axios.get('/src/json/localization/ru-Ru.json').then(response => this.state.ruLocale = response.data);
-    axios.get('/src/json/localization/en-En.json').then(response => this.state.enLocale = response.data);
+    axios.get('/src/json/localization/ru-Ru.json').then(response => this.state.ruLocale = response.data).catch(error => console.log(error));
+    axios.get('/src/json/localization/en-En.json').then(response => this.state.enLocale = response.data).catch(error => console.log(error));
   }
   
   switchLanguage = (locale) => {
